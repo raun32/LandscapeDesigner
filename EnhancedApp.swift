657a1922fa3ae026646e829ad4ef48d77f453e75
macguy <macguy@macguys-mbp.mynetworksettings.com>
2025-12-02 21:28:32 -0500

    fix: Remove textColor property from NSButton (not supported)
    NSButton doesn't have textColor property in AppKit. Button appearance
    controlled via layer backgroundColor only.

--- EnhancedApp.swift
+++ EnhancedApp.swift
@@ -1202,11 +1202,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             if penToolActive {
                 button.title = "✏️ Pen Tool (ON)"
                 button.layer?.backgroundColor = NSColor(red: 0.0, green: 0.8, blue: 0.2, alpha: 1).cgColor
-                button.textColor = NSColor.white
             } else {
                 button.title = "✏️ Pen Tool (OFF)"
                 button.layer?.backgroundColor = NSColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1).cgColor
-                button.textColor = NSColor.black
             }
         }
         
